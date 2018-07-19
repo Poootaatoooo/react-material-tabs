49ecbc0847a596a2a179820d32eb413d3239f969
Limit <drpotato27@gmail.com>
2018-07-19 14:54:37 -0700

    Initial Commit

---
+++ src/Tab.js
@@ -0,0 +1,51 @@
+'use strict';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { emptyFunc } from './utils';
+
+const Style = {
+	container: {
+		position: 'relative',
+		height: 48,
+		padding: 0,
+		cursor: 'pointer'
+	},
+
+	textContainer: {
+		position: 'relative',
+		height: '100%',
+		width: '100%',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		textAlign: 'center',
+		textTransform: 'uppercase',
+		fontSize: 14,
+
+		// Without this, Safari briefly fades the text color whilst ::active.
+		WebkitFontSmoothing: 'subpixel-antialiased'
+	}
+};
+
+const Tab = ({ children, onClick, style }) => {
+	return (
+		<div style={Style.container} onClick={onClick}>
+			<div style={{ ...Style.textContainer, ...style }}>
+				{children}
+			</div>
+		</div>
+	);
+};
+
+Tab.propTypes = {
+	style: PropTypes.object,
+	onClick: PropTypes.func
+};
+
+Tab.defaultProps = {
+	style: {},
+	onClick: emptyFunc
+};
+
+export default Tab;
